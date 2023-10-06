22e94b12aaa96d8a0d6f4d9eef55485c306c00d2
MohaMed AshRaf <48410724+mohamedashrraf@users.noreply.github.com>
2023-10-07 02:16:56 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Component } from 'react';
+
+import Home from "./components/Home/Home.jsx"
+import Parent from './components/Parent/Parent';
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Parent />
+    </div>
+  );
+}
+
+export default App;
